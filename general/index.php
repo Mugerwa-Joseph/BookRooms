032fa1459c360d0a390db24dac09dfaf7ab399cd
Mugerwa Joseph <mugsjsp21@gmail.com>
2018-10-02 10:02:46 +0300

    Update index.php

--- general/index.php
+++ general/index.php
@@ -14,12 +14,12 @@
 <?php
 
 // Inialize session
-session_start();
+//session_start();
 
 // Check, if username session is NOT set then this page will jump to login page
-if (!isset($_SESSION['username'])) {
-header('Location: login.php');
-}
+//if (!isset($_SESSION['username'])) {
+//header('Location: login.php');
+//}
 
 ?>
 
